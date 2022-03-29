9c839d321902bad29a5fcb0f10c70cb4a37f1d22
MonishaMohan30 <58160442+MonishaMohan30@users.noreply.github.com>
2022-03-29 16:49:48 +0530

    Add files via upload

---
+++ RecursionSelectionSort.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+import java.util.*;
+public class RecursionSelectionSort{
+    public static void main(String[] args) {
+     int[] arr  = {4,3,2,1};
+     selection(arr, arr.length, 0, 0);  
+     System.out.println(Arrays.toString(arr)); 
+    }
+
+    private static void selection(int[] arr,int r,int c,int max){
+     if(r == 0){
+         return;
+     }
+     if(c < r){
+         if(arr[c] > arr[max]){
+             selection(arr, r, c + 1, c);
+         }else{
+            selection(arr, r, c + 1, max);
+         }
+     }else{
+         int temp = arr[max];
+         arr[max] = arr[r-1];
+         arr[r-1] = temp;
+         selection(arr, r-1,0,0);
+     }
+    } 
+}
