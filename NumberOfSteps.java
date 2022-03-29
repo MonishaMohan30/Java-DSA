e8b9d5824ae7b9edb1c8da5a1e8ea31db63dee8e
MonishaMohan30 <58160442+MonishaMohan30@users.noreply.github.com>
2022-03-29 11:46:17 +0530

    Add files via upload

---
+++ NumberOfSteps.java
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+public class NumberOfSteps{
+    public static void main(String[] args){
+        System.out.println(numberOfSteps(7)); 
+    }
+
+   public static int numberOfSteps(int num) {
+        return helper(num, 0);
+    }
+
+    public static int helper(int num, int steps){
+        if(num == 0){
+            return steps;
+        }
+        if(num%2 == 0){
+            return helper(num/2, steps + 1);
+        }
+         return helper(num-1, steps + 1);
+    }
+}
