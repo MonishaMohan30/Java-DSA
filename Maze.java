acf805621a7d8f2ac6621fc898eaef9a551c64a0
MonishaMohan30 <58160442+MonishaMohan30@users.noreply.github.com>
2022-04-06 09:37:05 +0530

    Add files via upload

--- Maze.java
+++ Maze.java
@@ -5,6 +5,7 @@ public class Maze{
         System.out.println(count(3,3));
         path("",3,3);
         System.out.println(pathRet("",3,3));
+        System.out.println(pathRetDiagonal("",3,3));
     }
 
     static int count(int r, int c){
@@ -43,6 +44,31 @@ public class Maze{
         if(c>1){
            list.addAll(pathRet(p + 'R',r,c-1));
         }
+        return list;
+    }
+
+    public static ArrayList<String> pathRetDiagonal(String p, int r, int c) {
+        
+        if(r == 1 && c ==1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        
+        ArrayList<String> list = new ArrayList<>();
+
+        if(r>1 && c>1){
+            list.addAll(pathRetDiagonal(p+'D', r-1,c-1));
+        }
+
+        if(r > 1){
+            list.addAll(pathRetDiagonal(p+'V',r-1,c));
+        }
+
+        if(c > 1){
+            list.addAll(pathRetDiagonal(p+'H',r,c-1));
+        }
+
         return list;
     }
 }
